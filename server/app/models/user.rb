219541c74859c048b01da473ebdb10a8bed3997a
Dylan Meskis <dmeskis@gmail.com>
2020-05-09 11:31:48 -0600

    Test user model

---
+++ server/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  strip_attributes
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -1,8 +1,28 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/rails"
+require 'mocha/minitest'
+require "database_cleaner"
+require "faker"
+
+module AroundEachTest
+  def before_setup
+    super
+    DatabaseCleaner.clean
+    DatabaseCleaner.start
+  end
+end
+
+DatabaseCleaner.strategy = :transaction
+
+class Minitest::Test
+  include AroundEachTest
+end
 
 class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
